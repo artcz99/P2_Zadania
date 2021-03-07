0d09452c510a736e2e29993d3d1e89a60f0e6222
artcz99 <arturczarnik@outlook.com>
2021-03-07 21:11:05 +0100

    removed namespace std

--- Zestaw_1/main.cpp
+++ Zestaw_1/main.cpp
@@ -3,12 +3,10 @@
 #include <iostream>
 #include <cstdlib>
 
-using namespace std;
-
 void print_arr(int *arr, int size) { //prints each value from dynamic array
     for (int i = 0; i < size; i++)
-        cout << *(arr + i) << ' ';
-    cout << endl;
+        std::cout << *(arr + i) << ' ';
+    std::cout << std::endl;
 }
 
 int *random_arr(int size) { //generates an array with random values
@@ -29,7 +27,7 @@ int max_val(int *arr, int size) { //returns maximal value from given array
 
 int *reverse_array(int *arr, int size) { //reverses given array
     int temp;
-    for(int i = 0; i < size/2; i++) {
+    for (int i = 0; i < size / 2; i++) {
         temp = *(arr + i);
         *(arr + i) = *(arr + size - 1 - i);
         *(arr + size - 1 - i) = temp;
@@ -41,9 +39,9 @@ int *reverse_array(int *arr, int size) { //reverses given array
 int *shift_left(int *arr, int size, int shift) {
     shift %= size;
     int temp;
-    for(int i = 0; i < shift; i++) {
+    for (int i = 0; i < shift; i++) {
         temp = *(arr);
-        for(int j = 1; j < size; j++) {
+        for (int j = 1; j < size; j++) {
             *(arr + j - 1) = *(arr + j);
         }
         *(arr + size - 1) = temp;
@@ -51,7 +49,7 @@ int *shift_left(int *arr, int size, int shift) {
     return arr;
 }
 
-int* bubble_sort(int *arr, int size) { //sorts given array using the bubble sort algorithm
+int *bubble_sort(int *arr, int size) { //sorts given array using the bubble sort algorithm
     int temp;
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size - 1; j++) {
@@ -66,11 +64,11 @@ int* bubble_sort(int *arr, int size) { //sorts given array using the bubble sort
 
 }
 
-int* merge_arrays(int *arr1, int size1, int *arr2, int size2) { //merges two sorted arrays into a sorted array
-    int* return_arr = new int [size1+size2];
+int *merge_arrays(int *arr1, int size1, int *arr2, int size2) { //merges two sorted arrays into a sorted array
+    int *return_arr = new int[size1 + size2];
     int i = 0, j = 0;
-    while(i < size1 or j < size2) {
-        if(*(arr1 + i) < *(arr2 + j)) {
+    while (i < size1 or j < size2) {
+        if (*(arr1 + i) < *(arr2 + j)) {
             if (i >= size1) {
                 *(return_arr + i + j) = *(arr2 + j);
                 j++;
@@ -107,78 +105,80 @@ int *multiply(int *arr1, int size1, int *arr2, int size2) { //multiplies polynom
 
 
 int main() {
-    cout << "Zadanie 0" << endl;
+    std::cout << "Zadanie 0" << std::endl;
     int size;
-    cout << "Podaj rozmiar tablicy: ";
-    cin >> size;
-    int* arr = random_arr(size);
-    cout << "Losowa tablica: ";
+    std::cout << "Podaj rozmiar tablicy: ";
+    std::cin >> size;
+    int *arr = random_arr(size);
+    std::cout << "Losowa tablica: ";
     print_arr(arr, size);
-    cout << endl;
+    std::cout << std::endl;
 
-    cout << "Zadanie 1" << endl;
-    cout << "Maksymalna wartosc to: " << max_val(arr, size) << endl;
-    cout << endl;
+    std::cout << "Zadanie 1" << std::endl;
+    std::cout << "Maksymalna wartosc to: " << max_val(arr, size) << std::endl;
+    std::cout << std::endl;
 
-    cout << "Zadanie 2" << endl;
+    std::cout << "Zadanie 2" << std::endl;
     arr = reverse_array(arr, size);
-    cout << "Odwrocona kolejnosc: " << max_val(arr, size) << endl;
+    std::cout << "Odwrocona kolejnosc: " << max_val(arr, size) << std::endl;
     print_arr(arr, size);
-    cout << endl;
+    std::cout << std::endl;
 
-    cout << "Zadanie 3" << endl;
+    std::cout << "Zadanie 3" << std::endl;
     int shift;
-    cout << "O ile pol w lewo przesunac elementy w tablicy: ";
-    cin >> shift;
+    std::cout << "O ile pol w lewo przesunac elementy w tablicy: ";
+    std::cin >> shift;
     arr = shift_left(arr, size, shift);
     print_arr(arr, size);
     delete[] arr;
-    cout << endl;
+    std::cout << std::endl;
 
-    cout << "Zadanie 4" << endl;
+    std::cout << "Zadanie 4" << std::endl;
     int size_n4, size_m4;
-    cout << "Podaj rozmiar tablicy n: ";
-    cin >> size_n4;
-    cout << "Podaj rozmiar tablicy m: ";
-    cin >> size_m4;
+    std::cout << "Podaj rozmiar tablicy n: ";
+    std::cin >> size_n4;
+    std::cout << "Podaj rozmiar tablicy m: ";
+    std::cin >> size_m4;
     int *arr_n4 = random_arr(size_n4);
     int *arr_m4 = random_arr(size_m4);
-    cout << "Tablica n (przed sortowaniem): ";
+    std::cout << "Tablica n (przed sortowaniem): ";
     print_arr(arr_n4, size_n4);
-    cout << "Tablica m (przed sortowaniem): ";
+    std::cout << "Tablica m (przed sortowaniem): ";
     print_arr(arr_m4, size_m4);
     arr_n4 = bubble_sort(arr_n4, size_n4);
     arr_m4 = bubble_sort(arr_m4, size_m4);
-    cout << "Tablica n (po sortowaniu): ";
+    std::cout << "Tablica n (po sortowaniu): ";
     print_arr(arr_n4, size_n4);
-    cout << "Tablica m (po sortowaniu): ";
+    std::cout << "Tablica m (po sortowaniu): ";
     print_arr(arr_m4, size_m4);
-    int merged_arr_size = size_n4+size_m4;
+    int merged_arr_size = size_n4 + size_m4;
     int *merged_arr = merge_arrays(arr_n4, size_n4, arr_m4, size_m4);
-    cout << "Tablica powstala w wyniku scalenia tablic n i m: ";
+    std::cout << "Tablica powstala w wyniku scalenia tablic n i m: ";
     print_arr(merged_arr, merged_arr_size);
     delete[] arr_n4;
     delete[] arr_m4;
-    cout << endl;
+    std::cout << std::endl;
 
-    cout << "Zadanie 5" << endl;
+    std::cout << "Zadanie 5" << std::endl;
     int size_n5, size_m5;
-    cout << "Podaj rozmiar tablicy n: ";
-    cin >> size_n5;
-    cout << "Podaj rozmiar tablicy m: ";
-    cin >> size_m5;
+    std::cout << "Podaj rozmiar tablicy n: ";
+    std::cin >> size_n5;
+    std::cout << "Podaj rozmiar tablicy m: ";
+    std::cin >> size_m5;
     int *arr_n5 = random_arr(size_n5);
     int *arr_m5 = random_arr(size_m5);
-    cout << "Tablica n: ";
+    std::cout << "Tablica n: ";
     print_arr(arr_n5, size_n5);
-    cout << "Tablica m: ";
+    std::cout << "Tablica m: ";
     print_arr(arr_m5, size_m5);
-    cout << "Wynik mnozenia wielomianow: ";
+    std::cout << "Wynik mnozenia wielomianow: ";
 
     print_arr(multiply(arr_n5, size_n5, arr_m5, size_m5), size_n5 + size_m5 - 1);
     delete[] arr_n5;
     delete[] arr_m5;
-    cout << endl;
+    std::cout << std::endl;
+
+    std::cout << "Zadanie 6" << std::endl;
 
 
 }
