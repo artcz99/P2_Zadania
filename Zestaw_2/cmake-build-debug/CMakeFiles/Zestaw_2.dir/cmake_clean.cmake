bce0f90b3eb1f9f49a5cb69372fbc60547fbac6e
artcz99 <arturczarnik@outlook.com>
2021-03-07 21:02:26 +0100

    .

---
+++ Zestaw_2/cmake-build-debug/CMakeFiles/Zestaw_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zestaw_2.dir/main.cpp.obj"
+  "Zestaw_2.exe"
+  "Zestaw_2.exe.manifest"
+  "Zestaw_2.pdb"
+  "libZestaw_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zestaw_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
